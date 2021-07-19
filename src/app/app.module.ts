a113d69cc7f75a62ee6d21029fd88e394b114cf4
KevinRaleie-dev <kkevraleie@gmail.com>
2021-07-19 23:08:07 +0200

    listed all jobs in separate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponentComponent } from './todo-component/todo-component.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ListingsComponent } from './listings/listings.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponentComponent,
     NotFoundComponent,
-    NavbarComponent
+    NavbarComponent,
+    ListingsComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     ReactiveFormsModule
   ],
---
+++ src/utils/job.ts
@@ -0,0 +1,8 @@
+export interface Job {
+    job_title: string;
+    company: string;
+    job_type: string;
+    salary: string;
+    category: string;
+    publication_date: string;
+}
